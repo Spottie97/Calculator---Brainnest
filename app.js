e18044156cef2b763b39447113b0ee41dd40af86
Reinhardt Erasmus <reinhardterasmus@gmail.com>
2023-02-09 15:41:01 +0200

    Updated Funcationality to using Multiple Operators

--- app.js
+++ app.js
@@ -6,20 +6,25 @@ let equal = document.querySelector('.btn-equal');
 buttons.forEach(function(button){
     button.addEventListener('click', function(e){
         let value = e.target.dataset.num;
-        screen.value += value;
+        if (value === '+' || value === '-' || value === '*' || value === '/') {
+            if (screen.value.includes('+') || screen.value.includes('-') || screen.value.includes('*') || screen.value.includes('/')) {
+                let answer = eval(screen.value);
+                screen.value = answer + value;
+            } else {
+                screen.value += value;
+            }
+        } else {
+            screen.value += value;
+        }
     })
 });
 
 equal.addEventListener('click', function(e){
-    if(screen.value === ''){
-        screen.value = '';
-    } else {
-        let answer = eval(screen.value);
-        screen.value = answer;
-    }
-})
+    let answer = eval(screen.value);
+    screen.value = answer;
+});
 
 clear.addEventListener('click', function(e){
     screen.value = '';
-})
+});
 
