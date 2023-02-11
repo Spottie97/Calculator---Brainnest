c44397d004eba4af87001e672f50acbe0edaea8f
Reinhardt Erasmus <reinhardterasmus@gmail.com>
2023-02-11 12:26:22 +0200

    Added Keyboard Functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let screen = document.querySelector('.screen');
 let buttons = document.querySelectorAll('.btn');
 let clear = document.querySelector('.btn-clear');
 let equal = document.querySelector('.btn-equal');
+let backspace = document.querySelector('.btn-backspace');
 
 buttons.forEach(function(button){
     button.addEventListener('click', function(e){
@@ -27,7 +28,34 @@ equal.addEventListener('click', function(e){
         }
     }
 });
+//Backspace Button
+backspace.addEventListener('click', function(e) {
+    screen.value = screen.value.slice(0, -1);
+});
+
 // Clear Button
 clear.addEventListener('click', function(e){
     screen.value = '';
+});
+
+//Keyboard Functionality
+document.addEventListener('keydown', function(e) {
+    let key = e.key;
+    if (!isNaN(key)) {
+        screen.value += key;
+    } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+        screen.value += key;
+    } else if (key === '.' && !screen.value.includes(".")) {
+        screen.value += key;
+    } else if (key === 'Backspace') {
+        screen.value = screen.value.slice(0, -1);
+    } else if (key === 'Enter') {
+        let answer = eval(screen.value);
+        if (answer === Infinity) {
+            screen.value = "Error: Don't divide by 0, it's rude.";
+        } else {
+            answer = Math.round(answer * 100) / 100;
+            screen.value = answer;
+        }
+    }
 });
